3fe8105fc558eb25e0010806c3218a85a996757e
rupa <rupa@lrrr.us>
2013-02-24 16:19:16 -0500

    test directly, instead of testing exit variable

--- z.sh
+++ z.sh
@@ -213,7 +213,8 @@ elif complete &> /dev/null; then
  complete -o filenames -C '_z --complete "$COMP_LINE"' ${_Z_CMD:-z}
  [ "$_Z_NO_PROMPT_COMMAND" ] || {
   # bash populate directory list. avoid clobbering other PROMPT_COMMANDs.
-  echo $PROMPT_COMMAND | grep -q "_z --add"
-  [ $? -gt 0 ] && PROMPT_COMMAND='_z --add "$(pwd '$_Z_RESOLVE_SYMLINKS' 2>/dev/null)" 2>/dev/null;'"$PROMPT_COMMAND"
+  echo $PROMPT_COMMAND | grep -q "_z --add" || {
+   PROMPT_COMMAND='_z --add "$(pwd '$_Z_RESOLVE_SYMLINKS' 2>/dev/null)" 2>/dev/null;'"$PROMPT_COMMAND"
+  }
  }
 fi
