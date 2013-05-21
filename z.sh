8aac576b7cfcc2ac4fbad7e298bcacc29a03b731
rupa <rupa@lrrr.us>
2013-05-20 20:50:19 -0400

    revert warning if z.sh is being exec'd and not sourced
    Because we can't reliably determine if we're really being exec'd.

--- z.sh
+++ z.sh
@@ -22,11 +22,6 @@
 #   * z -l foo  # list matches instead of cd
 #   * z -c foo  # restrict matches to subdirs of $PWD
 
-case $- in
- *i*) ;;
-   *) echo 'ERROR: z.sh is meant to be sourced, not directly executed.'
-esac
-
 [ -d "${_Z_DATA:-$HOME/.z}" ] && {
     echo "ERROR: z.sh's datafile (${_Z_DATA:-$HOME/.z}) is a directory."
 }
