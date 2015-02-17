rupa/z

commit c507bdd7071e81ce9446ee63a3d6571908159c56

also ignore subdirs of _Z_EXCLUDE_DIRS

z.sh
@@ -41,10 +41,10 @@ _z() {
         # $HOME isn't worth matching
         [ "$*" = "$HOME" ] && return
 
-        # don't track excluded dirs
+        # don't track excluded directory trees
         local exclude
         for exclude in "${_Z_EXCLUDE_DIRS[@]}"; do
-            [ "$*" = "$exclude" ] && return
+            case "$*" in $exclude*) return;; esac
         done
 
         # maintain the data file

README
@@ -60,7 +60,7 @@ NOTES
               Set $_Z_NO_RESOLVE_SYMLINKS to prevent symlink resolution.
               Set $_Z_NO_PROMPT_COMMAND to handle PROMPT_COMMAND/precmd  your-
               self.
-              Set $_Z_EXCLUDE_DIRS to an array of directories to exclude.
+              Set $_Z_EXCLUDE_DIRS to an array of directory trees to  exclude.
               Set $_Z_OWNER to allow usage when in 'sudo -s' mode.
               (These  settings  should  go  in  .bashrc/.zshrc before the line
               added above.)
@@ -121,8 +121,8 @@ ENVIRONMENT
        to handle PROMPT_COMMAND or precmd yourself.
 
        The  environment  variable  $_Z_EXCLUDE_DIRS  can be set to an array of
-       directories to exclude from tracking. $HOME is always excluded.  Direc-
-       tories must be full paths without trailing slashes.
+       directory trees to exclude from tracking.  $HOME  is  always  excluded.
+       Directories must be full paths without trailing slashes.
 
        The  environment  variable  $_Z_OWNER  can  be set to your username, to
        allow usage of z when your sudo enviroment keeps $HOME set.

z.1
@@ -81,7 +81,7 @@ Set \fB$_Z_NO_RESOLVE_SYMLINKS\fR to prevent symlink resolution.
 Set \fB$_Z_NO_PROMPT_COMMAND\fR to handle \fBPROMPT_COMMAND/precmd\fR yourself.
 .RE
 .RS
-Set \fB$_Z_EXCLUDE_DIRS\fR to an array of directories to exclude.
+Set \fB$_Z_EXCLUDE_DIRS\fR to an array of directory trees to exclude.
 .RE
 .RS
 Set \fB$_Z_OWNER\fR to allow usage when in 'sudo -s' mode.
@@ -147,7 +147,7 @@ The environment variable \fB$_Z_NO_PROMPT_COMMAND\fR can be set if you want to
 handle \fBPROMPT_COMMAND\fR or \fBprecmd\fR yourself.
 .P
 The environment variable \fB$_Z_EXCLUDE_DIRS\fR can be set to an array of
-directories to exclude from tracking. \fB$HOME\fR is always excluded.
+directory trees to exclude from tracking. \fB$HOME\fR is always excluded.
 Directories must be full paths without trailing slashes.
 .P
 The environment variable \fB$_Z_OWNER\fR can be set to your username, to

commit 054cf43fbe101ab9870a146ebe61f1dbaf6fe56c

commit c507bdd broke zsh -- this fixes it

z.sh
@@ -44,7 +44,7 @@ _z() {
         # don't track excluded directory trees
         local exclude
         for exclude in "${_Z_EXCLUDE_DIRS[@]}"; do
-            case "$*" in $exclude*) return;; esac
+            case "$*" in "$exclude*") return;; esac
         done
 
         # maintain the data file